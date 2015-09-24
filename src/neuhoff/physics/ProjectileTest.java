dba784d10977c49ab4215ea6f78ea41862e829bd
Ahuva <ahuvanewoff112@gmail.com>
2015-09-23 22:52:33 -0400

    unknown changes

--- src/neuhoff/physics/ProjectileTest.java
+++ src/neuhoff/physics/ProjectileTest.java
@@ -12,5 +12,13 @@ public class ProjectileTest {
 		
 		Assert.assertEquals(27.81, x, 0.01);
 	}
+	
+	@Test
+	public void testGetY() {
+		Projectile p = new Projectile(31, 20, 2.7);
+		double y = p.getY();
+		
+		Assert.assertEquals(10.56, y, 0.01);
+	}
 }
 
