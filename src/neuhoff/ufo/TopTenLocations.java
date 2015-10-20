e13808d9bdf129b027dc645c408a4263e60b876d
Ahuva <ahuvanewoff112@gmail.com>
2015-10-20 00:33:02 -0400

    added TopTenLocations UFO

--- src/neuhoff/ufo/TopTenLocations.java
+++ src/neuhoff/ufo/TopTenLocations.java
@@ -3,7 +3,11 @@ package neuhoff.ufo;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import com.google.gson.Gson;
 
 public class TopTenLocations {
@@ -13,11 +17,40 @@ public class TopTenLocations {
 				"./ufo_awesome.json"));
 		
 		Gson gson = new Gson();
-		//this will create a gson UFOSightingList
-		UFOSightingList list = gson.fromJson(in, location);
-		//find top ten location and print in order
-		System.out.println(list.size());
 		
+		//this will create a Gson UFOSightingList
+		UFOSightingList list = gson.fromJson(in, UFOSightingList.class);
+		//find top ten location and print in order
 		in.close();
+		
+		//maps location to occurences
+		HashMap<String, Integer> count = new HashMap<String, Integer>();
+		String key = null;
+		for(int i = 0; i < list.size(); i++){
+			key = list.get(i).getLocation().trim();
+			int counter = count.containsKey(key) ? count.get(key) : 0;
+			count.put(key, counter + 1);
+		}
+		
+		//reverces the order to get the location from the number of occurences
+		HashMap<Integer, String> rev = new HashMap<Integer, String>();
+	    	for(Map.Entry<String, Integer> entry : count.entrySet()){
+	    		rev.put(entry.getValue(), entry.getKey());
+	    }
+		
+	    List<Integer> countList = new ArrayList<Integer>(count.values());
+		Collections.sort(countList, Collections.reverseOrder());
+		
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < 10; i++){
+			sb.append(i + 1);
+			sb.append(". ");
+			sb.append(rev.get(countList.get(i)));
+			sb.append(" - ");
+			sb.append(countList.get(i));
+			sb.append(" sightings\n");
+		}
+		System.out.println(sb.toString());
+		
 	}
 }
--- src/neuhoff/ufo/UFOSighting.java
+++ src/neuhoff/ufo/UFOSighting.java
@@ -8,8 +8,38 @@ public class UFOSighting {
 	private String duration;
 	private String description;
 
-	public UFOSighting() {
+	public UFOSighting(String sightedAt, String reportedAt, String location, String shape,
+			String duration, String description) {
+		this.sightedAt = sightedAt;
+		this.reportedAt = reportedAt;
+		this.location = location;
+		this.shape = shape;
+		this.duration = duration;
+		this.description = description;
+	}
 
+	public String getSightedAt() {
+		return sightedAt;
+	}
+
+	public String getReportedAt() {
+		return reportedAt;
+	}
+
+	public String getLocation() {
+		return this.location.trim();
+	}
+
+	public String getShape() {
+		return shape;
+	}
+
+	public String getDuration() {
+		return duration;
+	}
+
+	public String getDescription() {
+		return description;
 	}
 
 }
